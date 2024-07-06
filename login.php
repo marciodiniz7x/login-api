5f6111edcf5aa0bbef926c427a5608631c7a4a22
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-07-06 14:15:35 -0300

    Updates

--- login.php
+++ login.php
@@ -1,5 +1,8 @@
-<form method="post">
-    <input type="text" name="login" required>
-    <input type="password" name="senha" required>
-    <input type="submit" name="acao" value="Enviar">
-</form>
+<div class="form-area">
+    <h1>Login</h1>
+    <form method="post">
+        <input type="text" name="login"  placeholder="Nome" required autocomplete="off">
+        <input type="password" name="senha" placeholder="Senha" required autocomplete="off">
+        <input type="submit" name="acao" value="Enviar" class="submit">
+    </form>
+</div>
