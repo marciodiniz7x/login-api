50cf7a81b650d1c555d3b7da04f2d7c9cce66991
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-07-07 14:55:07 -0300

    Update register.php

---
+++ register.php
@@ -0,0 +1,13 @@
+<div class="form-area">
+    <h1>Cadastrar</h1>
+    <form method="post">
+        <input type="text" name="login" placeholder="Usuário" required autocomplete="off">
+        <input type="password" name="senha" placeholder="Senha" required autocomplete="off">
+        <input type="password" name="confirmar" placeholder="Confirmar senha" required autocomplete="off">
+        <input type="submit" name="acao" value="Cadastrar" class="submit">
+        <div class="new-user">
+            <span>Já é cadastrado?</span>
+            <a href="?register">Fazer login</a>
+        </div>
+    </form>
+</div>
