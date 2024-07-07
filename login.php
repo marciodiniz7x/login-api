264c731a97cb9d85880ad90f485a23e847c4aafa
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-07-07 14:55:48 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <div class="form-area">
     <h1>Login</h1>
     <form method="post">
-        <input type="text" name="login"  placeholder="Nome" required autocomplete="off">
+        <input type="text" name="login"  placeholder="Usuário" required autocomplete="off">
         <input type="password" name="senha" placeholder="Senha" required autocomplete="off">
         <input type="submit" name="acao" value="Login" class="submit">
         <div class="new-user">
             <span>Não tem conta?</span>
-            <a href="register.php">Cadastrar</a>
+            <a href="?register">Cadastrar</a>
         </div>
     </form>
     <?php 
